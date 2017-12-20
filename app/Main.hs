b4c48439392d380a1c223e2d5bb31015cc8aa7b9
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2017-12-21 12:08:56 +1300

    Implement basic symbol & space parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Lib (schleem)
 
 main :: IO ()
-main = someFunc
+main = schleem
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
-    ( someFunc
+    ( schleem
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+schleem :: IO ()
+schleem = do
+  (expr:_) <- getArgs
+  putStrLn $ readExpr expr
+
+
+readExpr :: String -> String
+readExpr input =
+  case parse (spaces >> symbol) "schleem" input of
+    Right val -> "Found value: " ++ show val
+    Left err -> "No match: " ++ show err
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+
+spaces :: Parser ()
+spaces = skipMany1 space
