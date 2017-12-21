9759989ef679cf369041b1f834f2fb37c9839c7a
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2017-12-22 09:28:29 +1300

    Implement some more basic binary operations

--- src/Eval.hs
+++ src/Eval.hs
@@ -28,6 +28,19 @@ primitives =
   , ("mod", numericBinop mod)
   , ("quotient", numericBinop quot)
   , ("remainder", numericBinop rem)
+  , ("=", numBoolBinop (==))
+  , ("<", numBoolBinop (<))
+  , (">", numBoolBinop (>))
+  , ("/=", numBoolBinop (/=))
+  , (">=", numBoolBinop (>=))
+  , ("<=", numBoolBinop (<=))
+  , ("&&", boolBoolBinop (&&))
+  , ("||", boolBoolBinop (||))
+  , ("string=?", strBoolBinop (==))
+  , ("string<?", strBoolBinop (<))
+  , ("string>?", strBoolBinop (>))
+  , ("string<=?", strBoolBinop (<=))
+  , ("string>=?", strBoolBinop (>=))
   ]
 
 
@@ -37,6 +50,40 @@ numericBinop _ singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op args = (Number . foldl1 op) <$> mapM unpackNum args
 
 
+numBoolBinop :: (Integer -> Integer -> Bool) -> [SchleemVal] -> ThrowsError SchleemVal
+numBoolBinop  = boolBinop unpackNum
+
+
+strBoolBinop :: (String -> String -> Bool) -> [SchleemVal] -> ThrowsError SchleemVal
+strBoolBinop  = boolBinop unpackStr
+
+
+boolBoolBinop :: (Bool -> Bool -> Bool) -> [SchleemVal] -> ThrowsError SchleemVal
+boolBoolBinop = boolBinop unpackBool
+
+
+boolBinop :: (SchleemVal -> ThrowsError a) -> (a -> a -> Bool) -> [SchleemVal] -> ThrowsError SchleemVal
+boolBinop unpacker op args =
+  if length args /= 2
+  then throwError $ NumArgs 2 args
+  else do
+    left <- unpacker $ head args
+    right <- unpacker $ args !! 1
+    return $ Bool $ left `op` right
+
+
 unpackNum :: SchleemVal -> ThrowsError Integer
 unpackNum (Number n) = return n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+
+unpackStr :: SchleemVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number n) = return $ show n
+unpackStr (Bool b) = return $ show b
+unpackStr notStr = throwError $ TypeMismatch "string" notStr
+
+
+unpackBool :: SchleemVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
